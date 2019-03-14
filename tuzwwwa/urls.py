d7d881141ee3f1cbb57323cdd21f4502743e0768
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-03-14 14:19:57 +0300

    start app sites

--- tuzwwwa/urls.py
+++ tuzwwwa/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from sites import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^logout/$', views.logout, {"next_page":'/'}),
+    url(r'^accounts/', include('registration.backends.simple.urls')), 
+    url(r'^tuzwwwa/', include('sites.urls')),
+
+    
 ]
