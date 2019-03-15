d3e24add0257bd42d477dc4004e29efcdd706223
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-03-15 12:12:40 +0300

    set up models

---
+++ sites/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+
+
+class ProjectForm(forms.Form):
+    '''creates instance of the form for posting projects
+    
+    Arguments:
+        forms {[type]} -- [description]
+    '''
+    
+
+
+class ProfileForm(forms.Form):
+    '''creates instance of the form for posting profile information
+    
+    Arguments:
+        forms {[type]} -- [description]
+    '''
+
+
+
+
+
+class RatingForm(forms.Form):
+    '''creates instance of the form for posting ratings on projects
+    
+    Arguments:
+        forms {[type]} -- [description]
+    '''
+
--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from django import forms
+
+
+class Project(models.Model):
+    '''creates instances of projects
+    
+    Arguments:
+        models {[type]} -- [description]
+    '''
+    title = models.CharField(max_length=255)
+    screenshot = models.ImageField(upload_to='screenshot/')
+    description = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.title
+
+    
+
+
+class Profile(models.Model):
+    '''creates instances of user profiles
+    
+    Arguments:
+        models {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    '''
+
+    avatar = models.ImageField(upload_to='avatar/')
+    bio = models.TextField(max_length=255)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    screenshot = models.ImageField(upload_to='screenshot/')
+    description = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    email = models.EmailField()
+
+
+    def __str__(self):
+        return self.avatar
+
+    
+
+
+
+
+
 
 
